c3947bc013446aacc6b44ba78f20270519e0b2a9
Jack Wines <jackwines@mac.com>
2019-03-19 22:02:49 -0400

    the beginnings of a posts view

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Control.Monad.IO.Class
 import Brick
 import Network.HTTP.Simple
 import Data.Aeson
@@ -12,6 +13,8 @@ import Graphics.Vty.Attributes
 import Data.Maybe
 import Brick.Widgets.Border
 import qualified Data.IntMap.Strict as M
+import Brick.Widgets.List
+import qualified Data.Vector as V
 import Types
 
 main :: IO ()
@@ -27,10 +30,23 @@ getTuiState = do
     topicsResp <- getResponseBody <$> httpJSON topicsRequest
     categoriesResp <- getResponseBody <$> httpJSON categoriesRequest
     return TuiState {
-                    cursor = topicListToCursor . topicList $ topicsResp,
+                    posts = Nothing,
+                    topics = Just $ list "contents" (V.fromList . topicList $ topicsResp) topicHeight,
                     userMap = M.fromList . map (\x -> (userId x, x)) . users $ topicsResp,
-                    categoryMap = M.fromList . map (\x -> (categoryId x, x)) . categories $ categoriesResp
+                    categoryMap = M.fromList . map (\x -> (categoryId x, x)) . categories $ categoriesResp,
+                    baseURL = "https://discourse.haskell.org/"
                     }
+
+
+getPosts :: TuiState -> IO (List String Post)
+getPosts (TuiState {baseURL = baseURL, topics = (Just topics)}) = do
+    let (Just selectedTopicID) = topicId . snd <$> listSelectedElement topics 
+    postsRequest <- parseRequest $ baseURL ++ "t/" ++ (show selectedTopicID) ++ ".json"
+    (PostResponse l) <- getResponseBody <$> httpJSON postsRequest
+    return $ list "posts" (V.fromList l) 5
+
+
+
 
 type ResourceName = String
 
@@ -46,57 +62,60 @@ tuiApp =
 
 
 drawTui :: TuiState -> [Widget ResourceName]
-drawTui (TuiState (NE.NonEmptyCursor prev curr next) userMap _) = (:[]) . viewport "posts" Vertical . vBox $
-        (map drawPost . reverse $ prev)
-     ++ (withAttr "selected" .  drawPost $ curr)
-      : (map drawPost next)
+drawTui (TuiState (Just scrollable) Nothing userMap categoryMap _) = (:[]) . renderList drawTopic True $ scrollable
     where
-        drawPost topic = border
-                        . (if (pinned topic) then  withAttr "pinned" else id)
+        drawTopic selected (Topic _ categoryId title likeCount postsCount posters pinned)= 
+                          (if selected then  withAttr "selected" . border else border)
+                        . (if pinned   then  withAttr "pinned"   else id)
                         . padRight Max
-                        $ (likes <+> title') <=> (postsCount' <+> poster)
+                        $ (likes <+> title') <=> hBox [category, postsCount', posters']
             where
                 likes :: Widget ResourceName
                 likes = padRight (Pad 1)
                       . hLimit 4
                       . padRight Max
-                      $ (str . show . likeCount $ topic)
+                      . str
+                      . show
+                      $ likeCount
 
                 title' :: Widget ResourceName
-                title' = withAttr "title" . str . title $ topic
+                title' = withAttr "title" . str $ title
 
                 postsCount' :: Widget ResourceName
                 postsCount' = padLeft (Pad 5)
                             . str
                             . ("posts: " ++)
                             . show
-                            . postsCount
-                            $ topic
+                            $ postsCount
 
-                poster :: Widget ResourceName
-                poster = padLeft (Pad 5)
+                posters' :: Widget ResourceName
+                posters' = padLeft (Pad 5)
                        . hBox
                        . mapFst (withAttr "OP") (withAttr "rest")
                        . showList
                        . map (\x ->  userName $ userMap M.! posterId x)
-                       . posters
-                       $ topic
+                       $ posters
+
+                category :: Widget ResourceName
+                category = padLeft (Pad 5) . str . categoryName $ categoryMap M.! categoryId
 
                 showList :: [String] -> [Widget ResourceName]
                 showList s = map str $ (map (++ " ") . init $ s) ++ [last s]
+drawTui (TuiState _ (Just posts) _ _ _) = (:[]) . renderList drawPost True $ posts
+    where
+        drawPost _ (Post id username contents) = border $ (withAttr "OP" . str $ username) <=> (strWrap $ contents)
 
-                mapFst :: (a -> a) -> (a -> a) ->  [a] -> [a]
-                mapFst fn fn' (x:xs) = (fn x) : (map fn' xs)
+mapFst :: (a -> a) -> (a -> a) ->  [a] -> [a]
+mapFst fn fn' (x:xs) = (fn x) : (map fn' xs)
 
-handleTuiEvent :: TuiState -> BrickEvent n e -> EventM n (Next TuiState)
-handleTuiEvent s (VtyEvent (EvKey key [])) = scroll s key 
-handleTuiEvent s e = halt s
+handleTuiEvent :: TuiState -> BrickEvent String e -> EventM String (Next TuiState)
+handleTuiEvent tui (VtyEvent (EvKey (KChar 'q') _)) = halt tui
+handleTuiEvent tui (VtyEvent (EvKey KRight  _)) = do
+    posts' <- liftIO $ getPosts tui
+    continue $ tui {posts = Just posts'}
 
-scroll s key = continue s {
-                          cursor = fromMaybe (cursor s) $ ((case key of
-                              KDown -> SNE.nonEmptyCursorSelectNext
-                              KUp   -> SNE.nonEmptyCursorSelectPrev) (cursor s))
-                      }
-
-topicListToCursor :: [Topic] -> NonEmptyCursor Topic
-topicListToCursor (x:xs) = NE.NonEmptyCursor [] x xs
+handleTuiEvent tui (VtyEvent (EvKey KLeft   _)) = continue $ tui {posts = Nothing}
+handleTuiEvent (TuiState (Just scrollable) Nothing usrMap catMap url) (VtyEvent ev) = continue . (\x -> TuiState (Just x) Nothing usrMap catMap url) =<< handler
+    where
+        handler :: EventM String (List String Topic)
+        handler = handleListEvent ev $ scrollable
--- src/Types.hs
+++ src/Types.hs
@@ -3,18 +3,18 @@ module Types where
 import Data.Aeson
 import Cursor.Simple.List.NonEmpty (NonEmptyCursor)
 import qualified Data.IntMap.Strict as M
-
+import Brick.Widgets.List
 
 instance FromJSON Topic where
     parseJSON = withObject "Topic" $ \v -> do 
-        postId' <- v .: "id"
+        topicId' <- v .: "id"
         title'  <- v .: "title"
         likeCount' <- v.: "like_count"
         postsCount' <- v.: "posts_count"
         posters' <- v.: "posters"
         pinned' <- v.: "pinned"
         categoryId' <- v.: "category_id"
-        return $ Topic postId' categoryId' title' likeCount' postsCount' posters' pinned'
+        return $ Topic topicId' categoryId' title' likeCount' postsCount' posters' pinned'
 
 instance FromJSON User where
     parseJSON (Object v) = User
@@ -42,9 +42,21 @@ instance FromJSON Category where
 
 instance FromJSON CategoryResponse where
     parseJSON = withObject "CategoryResponse" $ \v -> do
-        categoryList <- v .: "categoryList"
+        categoryList <- v .: "category_list"
         categories' <- categoryList .: "categories"
         return $ CategoryResponse categories'
+
+instance FromJSON PostResponse where
+    parseJSON = withObject "PastResponse" $ \v -> do
+        postStream <- v .: "post_stream"
+        posts' <- postStream .: "posts"
+        return $ PostResponse posts'
+
+instance FromJSON Post where
+    parseJSON (Object v) = Post
+        <$> v .: "id"
+        <*> v .: "username"
+        <*> v .: "cooked"
 
 newtype CategoryResponse = CategoryResponse {
                                          categories :: [Category]
@@ -56,35 +68,57 @@ data TopicResponse = TopicResponse
     topicList :: [Topic]
     } deriving (Show)
 
-data Topic = Topic {
-                   postId :: Int,
-                   categoryID :: Int,
-                   title :: String,
-                   likeCount :: Int,
-                   postsCount :: Int,
-                   posters :: [Poster],
-                   pinned :: Bool
-                   } deriving (Show)
+topicHeight :: Int
+topicHeight = 4
 
-data User = User {
-                 userId :: Int,
-                 userName :: String,
-                 realName :: String
-                 } deriving (Show)
+data Topic = Topic
+    {
+    topicId :: Int,
+    categoryID :: Int,
+    title :: String,
+    likeCount :: Int,
+    postsCount :: Int,
+    posters :: [Poster],
+    pinned :: Bool
+    } deriving (Show)
 
-data Category = Category {
-                         categoryId :: Int,
-                         categoryName :: String
-                         } deriving (Show)
+data User = User
+    {
+    userId :: Int,
+    userName :: String,
+    realName :: String
+    } deriving (Show)
 
-data Poster = Poster {
-                     posterId :: Int,
-                     description :: String
-                     } deriving (Show)
+data Category = Category
+    {
+    categoryId :: Int,
+    categoryName :: String
+    } deriving (Show)
 
-data TuiState =
-    TuiState {
-              cursor :: NonEmptyCursor Topic,
-              userMap :: M.IntMap User,
-              categoryMap :: M.IntMap Category
-             } deriving (Show)
+data Poster = Poster
+    {
+    posterId :: Int,
+    description :: String
+    } deriving (Show)
+
+data PostResponse = PostResponse
+    {
+    postList :: [Post]                         
+    } 
+
+data Post = Post
+    {
+    postId :: Int,
+    opUserName :: String,
+    contents :: String
+    } deriving (Show)
+
+
+data TuiState = TuiState
+    {
+    topics :: Maybe (List String Topic),
+    posts :: Maybe (List String Post),
+    userMap :: M.IntMap User,
+    categoryMap :: M.IntMap Category,
+    baseURL :: String
+    } deriving (Show)
